7a62f0b6cbff55e72b53928c1db235fad82f8886
panicboy <asasaki@gmail.com>
2016-08-11 16:28:13 -1000

    GraphQLList of counters

--- schema.js
+++ schema.js
@@ -3,11 +3,37 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLID
 } = require('graphql');
 
 let counter = 3;
 let counters = [42, 43];
+let counterObj = {
+  id : 55,
+  value : 42
+};
+let countersObj = [
+  { id : 550,
+    value : 43
+  },
+  { id : 551,
+    value : 44
+  }
+];
+
+const CounterObjType = new GraphQLObjectType({
+  name : 'CounterObj',
+  fields : {
+    id : {
+      type : GraphQLID
+    },
+    value : {
+      type : GraphQLInt
+    }
+  }
+});
+
 
 const queryType = new GraphQLObjectType({
   name : 'RootQuery',
@@ -23,6 +49,14 @@ const queryType = new GraphQLObjectType({
     counters : {
       type : new GraphQLList(GraphQLInt),
       resolve : () => counters
+    },
+    counterObj: {
+      type: CounterObjType,
+      resolve: () => counterObj
+    },
+    countersObj: {
+      type: new GraphQLList(CounterObjType),
+      resolve: () => countersObj
     }
   }
 });
